42447c4413714469e026736d1769a5d86c226ca5
Anuj Patel <anuj.patel@mindinventory.com>
2021-10-13 15:06:27 +0530

    [config] - added images and gif file in assets folder.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -71,7 +71,7 @@ class _AnimatedSearchBarExampleState extends State<AnimatedSearchBarExample> {
                         child: AnimatedSearchbar(
                           textEditingController: TextEditingController(),
                           isOriginalAnimation: true,
-                          enableKeyboardFocus: false,
+                          enableKeyboardFocus: true,
                         ),
                       ),
                     ],
